fdd0d8aa7073f01131e0501c5b8d306f4cf3e840
nereadelolmosanz <nereadelolmosanz@gmail.com>
2017-03-27 14:38:41 +0200

    ejercicio resuelto

--- js/texto.js
+++ js/texto.js
@@ -1,7 +1,25 @@
-var cargarTexto = function(){
-
-};
-
-
 //se ejecuta cuando está cargado el DOM
-jQuery(document).ready(cargarTexto);
+jQuery(document).ready(function(){
+  $.ajax({
+    type: "GET",
+    url: "textos/texto.txt",
+    cache: false
+  }).done(function( text ) {
+    console.log("texto 1 cargado");
+    $("#texto").html(text);
+  });
+
+
+  $("#getText").click(function(){
+	  $.ajax({
+	    type: "GET",
+	    url: "textos/texto2.txt",
+	    cache: false
+	  }).done(function( text ) {
+        console.log("texto 2 cargado");
+
+	    $("#texto").html(text);
+	  });
+  });
+
+});
